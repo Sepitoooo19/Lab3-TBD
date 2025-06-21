a5144c8134f383a5b16cbd602058660eccfb606e
Sepitoooo19 <benjamin.sepulveda@usach.cl>
2025-06-20 20:05:34 -0400

    CRUD de company

---
+++ Lab3/src/main/java/bdavanzadas/Lab3/repositories/CompanyRepository.java
@@ -0,0 +1,27 @@
+package bdavanzadas.Lab3.repositories;
+
+import bdavanzadas.Lab3.entities.CompanyEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CompanyRepository extends MongoRepository<CompanyEntity, String> {
+
+
+    // Búsqueda por RUT (único)
+    Optional<CompanyEntity> findByRut(String rut);
+
+    // Búsqueda por tipo de compañía
+    List<CompanyEntity> findByType(String type);
+
+    // Búsqueda por método de pago
+    @Query("{ 'paymentMethodIds': ?0 }")
+    List<CompanyEntity> findByPaymentMethodId(String paymentMethodId);
+
+    // Búsqueda por área de cobertura
+    @Query("{ 'coverageAreaIds': ?0 }")
+    List<CompanyEntity> findByCoverageAreaId(String coverageAreaId);
+}
