5930bfb425ec7debae2325335e9616a86363e533
Sepitoooo19 <benjamin.sepulveda@usach.cl>
2025-06-22 21:53:24 -0400

    Nuevo CRUD client document

--- lab1/src/main/java/bdavanzadas/lab1/documentControllers/ClientDocumentController.java
+++ lab1/src/main/java/bdavanzadas/lab1/documentControllers/ClientDocumentController.java
@@ -1,4 +1,69 @@
 package bdavanzadas.lab1.documentControllers;
 
+import bdavanzadas.lab1.documents.ClientDocument;
+import bdavanzadas.lab1.documentServices.ClientDocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/documents/clients")
 public class ClientDocumentController {
+
+    private final ClientDocumentService clientService;
+
+    @Autowired
+    public ClientDocumentController(ClientDocumentService clientService) {
+        this.clientService = clientService;
+    }
+    //getAllClients
+
+    @GetMapping
+    public ResponseEntity<Iterable<ClientDocument>> getAllClients() {
+        return ResponseEntity.ok(clientService.getAllClients());
+    }
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<ClientDocument> getByUserId(@PathVariable String userId) {
+        return clientService.getClientByUserId(userId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/rut/{rut}")
+    public ResponseEntity<ClientDocument> getByRut(@PathVariable String rut) {
+        return clientService.getClientByRut(rut)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{id}/name")
+    public ResponseEntity<String> getNameById(@PathVariable String id) {
+        String name = clientService.getClientNameById(id);
+        return name != null ? ResponseEntity.ok(name) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/exists/{clientId}")
+    public ResponseEntity<Boolean> existsByClientId(@PathVariable Integer clientId) {
+        return ResponseEntity.ok(clientService.existsByClientId(clientId));
+    }
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<ClientDocument> getByClientId(@PathVariable Integer clientId) {
+        return clientService.getClientByClientId(clientId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDocument> createClient(@RequestBody ClientDocument clientDocument) {
+        return ResponseEntity.ok(clientService.saveClient(clientDocument));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable String id) {
+        clientService.deleteClient(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- lab1/src/main/java/bdavanzadas/lab1/documentServices/ClientDocumentService.java
+++ lab1/src/main/java/bdavanzadas/lab1/documentServices/ClientDocumentService.java
@@ -1,4 +1,53 @@
 package bdavanzadas.lab1.documentServices;
 
+import bdavanzadas.lab1.documents.ClientDocument;
+import bdavanzadas.lab1.documentRepositories.ClientDocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClientDocumentService {
+
+    private final ClientDocumentRepository clientRepository;
+
+    @Autowired
+    public ClientDocumentService(ClientDocumentRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    //findAll
+    public List<ClientDocument> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    public Optional<ClientDocument> getClientByUserId(String userId) {
+        return clientRepository.findByUserId(userId);
+    }
+
+    public Optional<ClientDocument> getClientByRut(String rut) {
+        return clientRepository.findByRut(rut);
+    }
+
+    public String getClientNameById(String id) {
+        return clientRepository.findNameById(id);
+    }
+
+    public boolean existsByClientId(Integer clientId) {
+        return clientRepository.existsByClientId(clientId);
+    }
+
+    public Optional<ClientDocument> getClientByClientId(Integer clientId) {
+        return clientRepository.findByClientId(clientId);
+    }
+
+    public ClientDocument saveClient(ClientDocument clientDocument) {
+        return clientRepository.save(clientDocument);
+    }
+
+    public void deleteClient(String id) {
+        clientRepository.deleteById(id);
+    }
 }
